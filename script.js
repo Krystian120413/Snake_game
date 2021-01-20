73744b5bb61cb334459d18f5902754fcce20495b
Krystian120413 <Krystian12053@gmail.com>
2021-01-20 19:15:43 +0100

    new update

--- script.js
+++ script.js
@@ -3,28 +3,50 @@ const ctx = canvas.getContext('2d');
 let positionX = 0;
 let positionY = 0;
 let timer;
+let snake = [
+    {x: 40, y: 0},
+    {x: 20, y: 0},
+    {x: 0, y: 0}
+];
+
+const start = () => {
+    //background
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, 600,  600);
+
+    //first rectangle
+    ctx.fillStyle = '#fff';
+    for(let i = 0; i < snake.length; i++){
+        ctx.fillRect(snake[i].x, snake[i].y, 20, 20);
+        console.log(snake[i].x, snake[i].y);
+    }
+}
+
 const move = (x, y) => {
     //background
     ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, 600,  600);
 
-    positionY += y;
-    positionX += x;
-    if(positionY >= 600) positionY = 0;
-    if(positionY < 0) positionY = 580;
-    if(positionX >= 600) positionX = 0;
-    if(positionX < 0) positionX = 580;
+    for(let i = 0; i < snake.length; i++){
+        snake[i].y += y;
+        snake[i].x += x;
+        if(snake[i].y >= 600) snake[i].y = 0;
+        if(snake[i].y < 0) snake[i].y = 580;
+        if(snake[i].x >= 600) snake[i].x = 0;
+        if(snake[i].x < 0) snake[i].x = 580;
 
-    //first rectangle
-    ctx.fillStyle = '#fff';
-    ctx.fillRect(positionX, positionY, 20, 20);
-    console.log(positionX,positionY);
+        //first rectangle
+        ctx.fillStyle = '#fff';
+        ctx.fillRect(snake[i].x, snake[i].y, 20, 20);
+        console.log(snake[i].x, snake[i].y);
+    }
 
     clearTimeout(timer);
     timer = setTimeout("move("+x+", "+y+")", 500);
 }
 
 //start
+start();
 move(0, 0);
 
 //moving
