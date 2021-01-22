67485b3393424d131eaf69f52a746d64397fe8de
Krystian120413 <Krystian12053@gmail.com>
2021-01-22 15:34:24 +0100

    y3v4d help us

--- script.js
+++ script.js
@@ -1,117 +1,92 @@
 const canvas = document.getElementById('snake');
 const ctx = canvas.getContext('2d');
-let positionX = 0;
-let positionY = 0;
-let timer;
-let snake = [
-    {x:0, y:0},
-    {x: 20, y:0},
-    {x: 40, y: 0},
-    {x: 60, y: 0},
-    {x: 80, y: 0},
-    {x: 100, y: 0}
-];
+
+const partSize = 20;
+let timer = null;
+let snake = [];
+let apple = [];
+
+function draw() {
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    snake.forEach(o => {
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillRect(o.x, o.y, partSize, partSize);
+    });
+
+    ctx.fillStyle = 'red';
+    ctx.fillRect(apple[0], apple[1], partSize, partSize);
+}
 
 const start = () => {
-    //background
-    ctx.fillStyle = '#000';
-    ctx.fillRect(0, 0, 600,  600);
+    snake = [];
 
-    //first rectangle
-    ctx.fillStyle = '#fff';
-    for(let i = 0; i < snake.length; i++){
-        switch(i){
-            case 1: ctx.fillStyle = '#fff';
-            break;
-            case 2: ctx.fillStyle = 'red';
-            break;
-            case 3: ctx.fillStyle = 'green';
-            break;
-            case 4: ctx.fillStyle = 'blue';
-            break;
-            case 5: ctx.fillStyle = 'cyan';
-            break;
-            default: ctx.fillStyle = 'yellow';
-        }
-        ctx.fillRect(snake[i].x, snake[i].y, 19, 19);
-        console.log(snake[i].x, snake[i].y);
-    }
+    snake.push({ x: 2 * partSize, y: 0 });
+    snake.push({ x: partSize, y: 0 });
+    snake.push({ x: 0, y: 0 });
+
+    draw();
 }
 
 const move = (x, y) => {
-    //background
-    ctx.fillStyle = '#000';
-    ctx.fillRect(0, 0, 600,  600);
-    
-    for(let i = 0; i < snake.length; i++){
-        if(i > 0){
-            snake[i-1].x = snake[i].x;
-            snake[i-1].y = snake[i].y;
-        }
-        snake[i].y += y;
-        snake[i].x += x;
-        if(snake[i].y >= 600) snake[i].y = 0;
-        if(snake[i].y < 0) snake[i].y = 580;
-        if(snake[i].x >= 600) snake[i].x = 0;
-        if(snake[i].x < 0) snake[i].x = 580;
-
-        //draw rectangle
-        ctx.fillStyle = '#fff';
-        switch(i){
-            case 1: ctx.fillStyle = '#fff';
-            break;
-            case 2: ctx.fillStyle = 'red';
-            break;
-            case 3: ctx.fillStyle = 'green';
-            break;
-            case 4: ctx.fillStyle = 'blue';
-            break;
-            case 5: ctx.fillStyle = 'cyan';
-            break;
-            default: ctx.fillStyle = 'yellow';
-        }
-        ctx.fillRect(snake[i].x, snake[i].y, 19, 19);
-        console.log(snake[0].x, snake[0].y);
+    // move parts
+    for(let i = snake.length - 1; i > 0; i--){
+        snake[i].x = snake[i - 1].x;
+        snake[i].y = snake[i - 1].y;
     }
-    
+
+    // move head
+    snake[0].x += x;
+    snake[0].y += y;
+
+    // teleport head to the other side of the screen
+    if(snake[0].x >= canvas.width) snake[0].x = 0;
+    if(snake[0].x < 0) snake[0].x = canvas.width - partSize;
+    if(snake[0].y >= canvas.height) snake[0].y = 0;
+    if(snake[0].y < 0) snake[0].y = canvas.height - partSize;
+
+    if(snake[0].x === apple[0] && snake[0].y === apple[1]){
+        fruit();
+    }
+
+    draw();
 }
-    const firstmove = (x, y) => {
 
-        for(let i = 0; i < snake.length; i++){
-            snake[i].y -= y;
-            snake[i].x -= x;
-            if(snake[i].y >= 600) snake[i].y = 0;
-            if(snake[i].y < 0) snake[i].y = 580;
-            if(snake[i].x >= 600) snake[i].x = 0;
-            if(snake[i].x < 0) snake[i].x = 580;
-        }
-        clearInterval(timer);
-        timer = setInterval("move(" + x + ", " + y + ")", 500);
-    }
+const fruit = () => {
+    let a = Math.floor(Math.random() * (canvas.width + 1));
+    let b = Math.floor(Math.random() * (canvas.height + 1))
+    a -= a % partSize;
+    b -= b % partSize;
+    apple = [a, b];
+}
 
-//start
 start();
-//timer = setInterval("move(20, 0)", 500);
+fruit();
 
 //moving
 const moving = (e) => {
     //down
     if(e.code === 'KeyS'){
-        firstmove(0, 20);
+        clearInterval(timer);
+        timer = setInterval("move(0, " + partSize + ")", 500);
     }
     //up
     else if(e.code === 'KeyW'){
-        firstmove(0, -20);
+        clearInterval(timer);
+        timer = setInterval("move(0, " + -partSize + ")", 500);
     }
     //left
     else if(e.code === 'KeyA') {
-        firstmove(-20, 0);
+        clearInterval(timer);
+        timer = setInterval("move(" + -partSize + ", 0)", 500);
     }
     //right
     else if(e.code === 'KeyD') {
-        firstmove(20, 0);
+        clearInterval(timer);
+        timer = setInterval("move(" + partSize + ", 0)", 500);
     }
-    else console.log("doesnt work");
+    
     console.log(e.code);
 }
 
